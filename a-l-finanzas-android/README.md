0edaa55bb88d80f00df8bb2618218b2460f70e7d
adriannotario25-cmyk <adriannotario25@gmail.com>
2026-08-16 01:26:58 -0300

    Add files via upload

---
+++ a-l-finanzas-android/README.md
@@ -0,0 +1,2 @@
+# A & L Finanzas Android
+Aplicación Android de A & L Finanzas. Usa la versión cloud publicada en Vercel y los datos compartidos en Supabase.
---
+++ a-l-finanzas-android/app/src/main/java/com/alfinanzas/app/MainActivity.java
@@ -0,0 +1,54 @@
+package com.alfinanzas.app;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    private static final String APP_URL = "https://a-l-finanzas.vercel.app";
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setCacheMode(WebSettings.LOAD_DEFAULT);
+        s.setAllowFileAccess(false);
+        s.setAllowContentAccess(false);
+        s.setSupportZoom(false);
+
+        CookieManager cm = CookieManager.getInstance();
+        cm.setAcceptCookie(true);
+        cm.setAcceptThirdPartyCookies(webView, true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+
+        if (savedInstanceState == null) webView.loadUrl(APP_URL);
+        else webView.restoreState(savedInstanceState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        webView.saveState(outState);
+        super.onSaveInstanceState(outState);
+    }
+}
